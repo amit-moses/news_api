dbe80facf6b00bb2551d134cc9117ceb0a53862b
amit-moses <amitm747@gmail.com>
2023-06-23 13:12:38 +0300

    added real db

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///news.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///news.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_HOST')
 db = SQLAlchemy(app)
 CORS(app)
 
@@ -31,8 +35,12 @@ def api(id=0, pg=0):
    if request.method == 'GET':
       if pg == 0:
          if id == 0: return jsonify([obj.toJSON() for obj in Article.query.all()])
-         else: return jsonify([obj.toJSON() for obj in Article.query.filter(Article.id.ilike(id)).all()])
-      else: return jsonify([obj.toJSON() for obj in Article.query.filter(Article.category.ilike(pg)).all()])
+         else: 
+            arc = Article.query.get(id)
+            if arc: 
+               return jsonify([arc.toJSON()])
+            else: return jsonify([])
+      else: return jsonify([obj.toJSON() for obj in Article.query.filter(Article.category.ilike(str(pg))).all()])
       
    if (request.method == 'POST' or request.method == 'PUT'):
       data = request.get_json()
@@ -40,30 +48,31 @@ def api(id=0, pg=0):
          new_article = Article(title=data['title'], content=data['content'], category=data['category'],image=data['image'])
          db.session.add(new_article)
          db.session.commit()
-         print('888')
          return jsonify({'message': '1'})
       
       if request.method == 'PUT' and id != 0:
-         res = Article.query.filter(Article.id.ilike(id)).all()
-         if len(res)>0:
-            print(res[0].toJSON())
-            res[0].title, res[0].content, res[0].image, res[0].category = data['title'], data['content'], data['image'], data['category'],
-            print(res[0].toJSON())
+         res = Article.query.get(id)
+         if res:
+            res.title, res.content, res.image, res.category = data['title'], data['content'], data['image'], data['category'],
             db.session.commit()
             return jsonify({'message': '1'})
    
    if request.method == 'DELETE':
-      res = Article.query.filter(Article.id.ilike(id)).all()
-      if len(res)>0:
-         db.session.delete(res[0])
+      res = Article.query.get(id)
+      if res:
+         db.session.delete(res)
          db.session.commit()
-         return jsonify({'message': '1', 'cat': res[0].category})
+         return jsonify({'message': '1', 'cat': res.category})
    return jsonify({'message': '0'})
 
 @app.route("/api/cat", methods=['POST', 'GET', 'PUT', 'DELETE'])
 def cat():
    return jsonify([obj.toJSON() for obj in Categorey.query.all()])
 
+def otg(data):
+      new_article = Article(title=data['title'], content=data['content'], category=data['category'],image=data['image'])
+      db.session.add(new_article)
+      db.session.commit()
    
 
 with app.app_context():
